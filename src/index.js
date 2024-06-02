b1cb73647876a5c4e22c13e612ffcc69d924d8b9
Anurag <anurag9953001342@gmail.com>
2024-06-02 16:27:06 +0530

    utils - apierror- apiresponse- asynchandler

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-
+import app from "./app.js"
 import dbconnection from "./db/index.js";
 import dotenv from "dotenv";
 
-dotenv.config({
+dotenv.config(({
     path:'./env'
+}))
+dbconnection()
+//.then .catch is a promise for async and await used in dbconnection
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running on port : ${process.env.PORT}`)
+    })
 })
-dbconnection();
+.catch((err)=>{
+    console.log(err)
+    })
